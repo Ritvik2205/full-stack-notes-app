64fab392e70961c3b6e26aba39ad5c3809e73eb6
Ritvik <ritvik.gupta2205@gmail.com>
2024-07-04 17:12:36 +0530

    minor cleaning up

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ from flask_pymongo import PyMongo
 # from .auth import get_current_user
 import datetime
 from dotenv import load_dotenv
-import mysql.connector
+# import mysql.connector
 
 load_dotenv()
 
